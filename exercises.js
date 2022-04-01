1a7455d24375a1cf372d7d9644ea2ae19a7669fa
bmpknight <phelpsbradley45@gmail.com>
2022-04-01 17:56:35 -0400

    April 1, 2022 5:56 PM

--- exercises.js
+++ exercises.js
@@ -1,27 +1,27 @@
-// What is a function in  JavaScript?
+// // What is a function in  JavaScript?
 
-function myFunction() {
-    return 1;
-}
+// function myFunction() {
+//     return 1;
+// }
 
 
-// Anon functiono
-let myFunc = function () {
-    return 1;
-}
+// // Anon functiono
+// let myFunc = function () {
+//     return 1;
+// }
 
-// Arrow Function directly
-let myFuncArrow = () => a + 1
-console.log(myFuncArrow(110))
+// // Arrow Function directly
+// let myFuncArrow = () => a + 1
+// console.log(myFuncArrow(110))
 
-// Arrow Function in block
-let myFuncArrow2 = () => ({ a: 5})
-console.log(myFuncArrow2())
+// // Arrow Function in block
+// let myFuncArrow2 = () => ({ a: 5})
+// console.log(myFuncArrow2())
 
-function myHigherOrderFunction(parameterFunction){
-    return parameterFunction()
-}
-console.log(myHigherOrderFunction(myFuncArrow2))
+// function myHigherOrderFunction(parameterFunction){
+//     return parameterFunction()
+// }
+// console.log(myHigherOrderFunction(myFuncArrow2))
 
 function repeat(n, action) {
     for (let i = 0; i < n; i++) {
@@ -35,10 +35,6 @@ function repeat(n, action) {
   let arrays = [[1,2,3], [4,5],[6]];
   // Your code here.
   // -> [1,2,3,4,5,6]`
-
- let arrays = [[1,2,3],[4,5],[6]];
-// Your code here
- // -> [1,2,3,4,5,6]
 
 export function flattening(inputList) {
     return inputList
