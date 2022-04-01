00285b0c02c6da91d9c584fb6100d2e8e495ff38
bmpknight <phelpsbradley45@gmail.com>
2022-04-01 16:14:34 -0400

    April 1, 2022 4:14 PM

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let quotes = [`"Do or do not, there is no try" - Yoda`,
 `"Never put off tomorrow what you can do today." - Thomas Jefferson`,
 `"Technology is the campfire around which we tell our stories." - Laurie Anderson`]
-console.log(Math.random())
-console.log(3 * Math.random())
-console.log(Math.floor((3 * Math.random())))
-let index = Math.floor((3 * Math.random()))
+//  console.log(Math.random())
+// console.log(3 * Math.random())
+// console.log(Math.floor((3 * Math.random())))
+let index = Math.floor((quotes.length * Math.random()))
 console.log(quotes[index])
 
