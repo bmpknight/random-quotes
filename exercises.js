d0074092f0ec5e7e0f29eabebd65b9156f060d88
bmpknight <phelpsbradley45@gmail.com>
2022-04-01 18:31:42 -0400

    April 1, 2022 6:31 PM

--- exercises.js
+++ exercises.js
@@ -50,7 +50,7 @@ export function loop(value, test, update, body){
 
 loop(3, n => n > 0, n => n - 1, console.log);
 
-function everyLoop(array, test) 
+export function everyLoop(array, test) 
 {
   // Your code here.
   let returnValue = true
@@ -60,14 +60,22 @@ function everyLoop(array, test)
   return returnValue
 }
 
-function everySome(array, test) 
-{
-// Your code here.
-} 
 
 console.log(everyLoop([1, 3, 5], n => n < 10));
 // → true
 console.log(everyLoop([2, 4, 16], n => n < 10));
 // → false
 console.log(everyLoop([], n => n < 10));
+// → true
+
+ export function everySome(array, test) 
+{
+// Your code here.
+array.some(test)
+} 
+console.log(everySome([1, 3, 5], n => n < 10));
+// → true
+console.log(everySome([2, 4, 16], n => n < 10));
+// → false
+console.log(everySome([], n => n < 10));
 // → true
